7474c8075127614a71267313d1a08193be3696cb
Bahriye Begum Ultan Gungor <begumultan@gmail.com>
2020-08-11 18:18:43 +0300

    First commit

---
+++ Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+// < , > , == , !=
+// AND &&
+// OR ||
+
+var myAge = 40
+
+if myAge < 30 {
+    print(" 30-")
+} else if myAge > 30 && myAge <= 40 {
+    print("30s")
+} else if myAge > 40 && myAge < 50{
+    print("40+")
+}else {
+    print("50+")
+}
+
+//AND
+3 < 5 && 5 < 7
+3 < 5 && 8 < 7
+//OR
+3 < 5 || 5 < 7
+
+
+var myString = "James"
+if myString == "James"{
+    print("yes")
+}else {
+    print("no")
+}
+
+
+
+
+
+
+
+
+
